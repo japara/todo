ea2a1666ed1e2fe3af2c30404100b07f24eb9955
japara <72559893+japara@users.noreply.github.com>
2024-11-01 22:02:53 +0400

    clerk start

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,13 +1,17 @@
-import "./App.css";
-// import SignIn from "./components/singin";
-import User from "./components/user";
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import SignInPage from "./components/singin";
+import User from "./components/user"; // Import your User component
 
 function App() {
   return (
-    <div className="App">
-      {/* <SignIn /> */}
-      <User />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/sign-in" element={<SignInPage />} />
+        <Route path="/user" element={<User />} />{" "}
+        {/* Add other routes as needed */}
+      </Routes>
+    </Router>
   );
 }
 
--- todo/src/index.js
+++ todo/src/index.js
@@ -3,11 +3,22 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { ClerkProvider } from "@clerk/clerk-react";
+
+// Import your publishable key
+const PUBLISHABLE_KEY = process.env.REACT_APP_CLERK_PUBLISHABLE_KEY;
+console.log(PUBLISHABLE_KEY);
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error("Add your Clerk publishable key to the .env.local file");
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
+      <App />
+    </ClerkProvider>
   </React.StrictMode>
 );
 
